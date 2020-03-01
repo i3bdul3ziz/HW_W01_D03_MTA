e578dfc691116f40679e1364b0ecadcfbf5f3b0a
Abdulaziz Althagafi <labdul2ziz@gmail.com>
2020-03-01 20:00:14 +0300

    update assignment

--- MTALAb.js
+++ MTALAb.js
@@ -1,140 +1,70 @@
 let lines = {
-    n : "N",
-    s : "Six",
-    l : "L",
-    nLine : ["Times Square", "34th", "28th", "23rd", "Union Square", "8th"],
-    lLine : ["8th", "6th", "Union Square", "3rd","1st"],
-    sixLine : ["GC","33rd", "28th", "23rd", "Union Square", "AP"],
-    changePoint :"Union Square"
+  N : ["Times Square","34th","28th","23rd","Union Square","8th"],
+  L : ["8th","6th","Union Square","3rd","1st"],
+  Six : ["Grand Central", "33rd", "28th", "23rd", "Union Square", "Astor Place"]
+}
+
+function firstLine(line,station){
+  let lineName = line
+  let indexOfStation = lineName.indexOf(station)
+  let indexOfUnion = lineName.indexOf("Union Square")
+  if(indexOfStation > indexOfUnion){
+    return stationAndUnion(lineName,indexOfUnion,indexOfStation)
   }
- function planTrip(start,init,eName,final){
-    let indexOfUnion
-    let count = 1
-    let str = ""
-    let str2 = ""
-    let b
-    let index
-    if( start === lines.n && init === lines.nLine[0] ){
-      if(eName === lines.l && final === lines.lLine[lines.lLine.length - 1]){
-        for(let i = 0; i < lines.nLine.length; i++){
-         b = lines.nLine.includes("Union Square")
-         if(b === true){
-            for( let j = 0; j < lines.lLine.length; j++){
-               b = lines.lLine.includes("Union Square")
-               if(b === true){
-                  index = j - 1
-                  str2 = lines.lLine.slice(index, lines.lLine.length)
-                  count = j
-               }
-            }
-            indexOfUnion = lines.nLine.indexOf(i) 
-            str = lines.nLine.slice(0, indexOfUnion)
-          count = count + i - 2
-         }
-        }
-      } else if(eName === lines.s && final === lines.sixLine[lines.sixLine.length - 1]){
-        count = 1
-        for(let i = 0; i < lines.nLine.length; i++){
-          b = lines.nLine.includes("Union Square")
-          if(b === true){
-            for( let j = 0; j < lines.sixLine.length; j++){
-              b = lines.sixLine.includes("Union Square")
-              if(b === true){
-                index = j 
-                str2 = lines.sixLine.slice(index, lines.sixLine.length)
-                count = j
-              }
-            }
-            indexOfUnion = lines.nLine.indexOf(i) 
-            str = lines.nLine.slice(0, indexOfUnion)
-            count = count + i - 4
-          }
-        }
-      }
-    } else if ( start === lines.l && init === lines.lLine[0] ){
-      if(eName === lines.n && final === lines.nLine[lines.nLine.length - 1]){
-        for(let i = 0; i < lines.lLine.length; i++){
-         b = lines.lLine.includes("Union Square")
-         if(b === true){
-            for( let j = 0; j < lines.nLine.length; j++){
-               b = lines.nLine.includes("Union Square")
-               if(b === true){
-                  index = j 
-                  str2 = lines.nLine.slice(index, lines.nLine.length)
-                  count = j
-               }
-            }
-            indexOfUnion = lines.lLine.indexOf(i) 
-            str = lines.lLine.slice(0, indexOfUnion - 1)
-          count = count + i - 5
-         }
-        }
-      } else if(eName === lines.s && final === lines.sixLine[lines.sixLine.length - 1]){
-        count = 1
-        for(let i = 0; i < lines.lLine.length; i++){
-          b = lines.lLine.includes("Union Square")
-          if(b === true){
-            for( let j = 0; j < lines.sixLine.length; j++){
-              b = lines.sixLine.includes("Union Square")
-              if(b === true){
-                index = j 
-                str2 = lines.sixLine.slice(index, lines.sixLine.length)
-                count = j
-              }
-            }
-            indexOfUnion = lines.lLine.indexOf(i) 
-            str = lines.lLine.slice(0, indexOfUnion - 1)
-            count = count + i - 5
-          }
-        }
-      }
-    } else if ( start === lines.s && init === lines.sixLine[0] ){
-        if(eName === lines.n && final === lines.nLine[lines.nLine.length - 1]){
-          for(let i = 0; i < lines.sixLine.length; i++){
-            b = lines.sixLine.includes("Union Square")
-            if(b === true){
-              for( let j = 0; j < lines.nLine.length; j++){
-                b = lines.nLine.includes("Union Square")
-                if(b === true){
-                  index = j 
-                  str2 = lines.nLine.slice(index, lines.nLine.length)
-                  count = j
-                }
-              }
-              indexOfUnion = lines.sixLine.indexOf(i) 
-              str = lines.sixLine.slice(0, indexOfUnion)
-              count = count + i - 4
-            }
-          }
-        } else if(eName === lines.l && final === lines.lLine[lines.lLine.length - 1]){
-          count = 1
-          for(let i = 0; i < lines.sixLine.length; i++){
-            b = lines.sixLine.includes("Union Square")
-            if(b === true){
-            for( let j = 0; j < lines.lLine.length; j++){
-              b = lines.lLine.includes("Union Square")
-              if(b === true){
-                index = j 
-                str2 = lines.lLine.slice(index, lines.lLine.length)
-                count = j
-              }
-            }
-            indexOfUnion = lines.sixLine.indexOf(i) 
-            str = lines.sixLine.slice(0, indexOfUnion )
-            count = count + i - 3
-          }
-        }
-      }
-    }
-    console.log("You must travel through the following stops on the " + start + " line: " + str + ".")
-    console.log("Change at " + lines.changePoint)
-    console.log("Your journey continues through the following stops: " + str2 + ".")
-    console.log(count + " stops in total.")
- }
-  
-  planTrip("N","Times Square","L","1st")
-  planTrip("N","Times Square","Six","AP")
-  planTrip("L","8th","N","8th")
-  planTrip("L","8th","Six","AP")
-  planTrip("Six","GC","N","8th")
-  planTrip("Six","GC","L","1st")
+  return lineName.slice(indexOfStation, indexOfUnion + 1)
+}
+
+function secondLine(line,station){
+  let lineName = line
+  let indexOfStation = lineName.indexOf(station)
+  let indexOfUnion = lineName.indexOf("Union Square")
+  if(indexOfStation < indexOfUnion){
+    return stationAndUnion(lineName, indexOfUnion, indexOfStation)
+  }
+  let r = lineName.slice(indexOfUnion, indexOfStation + 1);
+  r.splice(r[indexOfUnion], 1);
+  return r;
+}
+
+function stationAndUnion(line, indexOfUnion, indexOfStation) {
+  if (indexOfStation > indexOfUnion) {
+    return line.slice(indexOfUnion, indexOfStation + 1).reverse();
+  } else {
+    var r = line.slice(indexOfStation, indexOfUnion).reverse();
+    r.splice(indexOfUnion, 1);
+    console.log(r)
+    return r;
+  }
+}
+
+function sameLine(line, start, end){
+  var lineName = line
+  var indexOfStart = lineName.indexOf(start)
+  var indexOfEnd = lineName.indexOf(end);
+
+  if (indexOfEnd < indexOfStart) {
+    return lineName.slice(indexOfEnd, indexOfStart + 1).reverse();
+  }
+  return lineName.slice(indexOfStart, indexOfEnd + 1);
+}
+
+function planTrip(startLine, startStation, endLine, endStation){
+  if(startLine === endLine){
+    let s = sameLine(startLine, startStation, endStation);
+    console.log("You must travel through the following stops on the N line: " + s.slice(1, s.length).join(", ") + ".")
+    let stops = s.length -1
+    console.log(stops + " stops in total.")
+  } else if (startLine !== endLine) {
+    let f1 = firstLine(startLine, startStation);
+    let f2 = secondLine(endLine, endStation);
+    let stops = f1.concat(f2).length - 1 
+    console.log("You must travel through the following stops : " + f1.slice(1, f1.length).join(", ") + ".")
+    console.log("Your journey continues through the following stops:" + f2.join(", ") + ".")
+    console.log("Then change at Union Square")
+    console.log(stops + " stops in total")
+  } else {
+    return "Invalid Parameters.";
+  }
+}
+
+planTrip(lines.N, "Times Square", lines.Six, "Grand Central")
